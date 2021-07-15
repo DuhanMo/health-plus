bb2907885254e91f46fb1355eaec404db8ece497
DuhanMo <devduhanmo@gmail.com>
2021-07-15 23:42:25 +0900

    setting

---
+++ src/test/java/org/duhan/healthplus/HealthPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.duhan.healthplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
